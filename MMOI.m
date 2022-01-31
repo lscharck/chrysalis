885fe344377c9a6141ea2a449a60c0b08f6ca40f
JustinianOfConstantinople <jfb0029@auburn.edu>
2022-01-31 12:09:50 -0600

    Created MMOI Matrix for arbitrary 6U Satellite

---
+++ MMOI.m
@@ -0,0 +1,10 @@
+function I = MMOI
+%Mass Moment of Inertia for Aribtrary 6U Cubesat
+%Formula from Wiki
+%From CubeSat.org ...
+m = 12; % Assumed Maximum [kg]
+x_dim = .2263; % [m]
+y_dim = .1000; % [m]
+z_dim = .3660; % [m]
+
+I = (m/12) * diag((y_dim^2 + z_dim^2),(x_dim^2 + z_dim^2),(x_dim^2 + y_dim^2));
