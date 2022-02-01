50f8475fad4f7fe5638be19b2707bbc956a53697
lscharck <lscharck@gmail.com>
2022-01-31 23:01:48 -0600

    Define Euler equations of motion function for ode45

---
+++ euler_eqs.m
@@ -0,0 +1,21 @@
+function [X_dot] = euler_eqs(t, X_0)
+
+    % Euler's rotation equations for no applied moments and symmetic body
+
+    % get mass moment of inertia for Cubesat
+    I = MMOI;
+    Ix = I(1,1);
+    Iy = I(2,2);
+    Iz = I(3,3);
+
+    X_dot = zeros(6,1);
+
+    X_dot(1) = X_0(4); 
+    X_dot(2) = X_0(5);
+    X_dot(3) = X_0(6);
+
+    X_dot(4) = (Iy - Iz) *  X_0(5) * X_0(6) / Ix; 
+    X_dot(5) = (Iz - Ix) *  X_0(6) * X_0(4) / Iy;
+    X_dot(6) = (Ix - Iy) *  X_0(4) * X_0(5) / Iz;
+
+end
