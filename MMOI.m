e5a0b35a14f8bc47f6e481028c295b3f221589d4
lscharck <lscharck@gmail.com>
2022-01-31 23:00:41 -0600

    Wrap diag arg in square brackets, it wanted them

--- MMOI.m
+++ MMOI.m
@@ -7,4 +7,4 @@ x_dim = .2263; % [m]
 y_dim = .1000; % [m]
 z_dim = .3660; % [m]
 
-I = (m/12) * diag((y_dim^2 + z_dim^2),(x_dim^2 + z_dim^2),(x_dim^2 + y_dim^2));
+I = (m/12) * diag([(y_dim^2 + z_dim^2),(x_dim^2 + z_dim^2),(x_dim^2 + y_dim^2)]);
