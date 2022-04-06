68341db9b44387009e4f695ac1616513bca15092
lscharck <lscharck@gmail.com>
2022-04-06 12:05:09 -0500

    Added a path for the newly renamed magnetic_field folder

--- model_startup.m
+++ model_startup.m
@@ -1,7 +1,7 @@
 %% Startup Script to pull things in from the Workspace
 home = pwd;
 addpath(strcat(home, '/model'));
-addpath(strcat(home, '/magnetorquer'));
+addpath(strcat(home, '/magnetic_field'));
 addpath(strcat(home, '/controller'));
 
 %Mass Moment of Inertia
