32330f36ca44365835f15f654a830b80ec257f7e
JustinianOfConstantinople <jfb0029@auburn.edu>
2022-04-19 09:57:08 -0500

    Preliminary failue counts added to milkweed app

--- monte_carlo/monte_carlo_util.m
+++ monte_carlo/monte_carlo_util.m
@@ -3,12 +3,12 @@
 %% Get Run Name
 prompt = {'Enter Name For Run', 'Sample Size (n = ?)', 'Margin'};
 dlgtitle = 'Run Info';
-dims = [1 35];
+dims = [1 40];
 definput = {'default name', '0', '0.0'};
 answers = inputdlg(prompt,dlgtitle,dims,definput);
 run_name = answers{1};
 n = str2num(answers{2});
-margin = str2num(answers{3}) + 1;
+margin = str2num(answers{3});
 if (strcmp(run_name,"default name") || n == 0)
        msgbox('Can you change the responses please? We will try this again later.')  
 end
@@ -37,8 +37,7 @@ expected_rates = deg2rad(5); %We expect 5 deg/s per axis
 % margin = 2;
 
 %Generating a matrix of rates in the range [-margin * rates, margin * rates]
-random_rates = margin * (-expected_rates + 2 * (expected_rates) * rand(n,3));
-
+random_rates = (margin+1) * (-expected_rates + 2 * (expected_rates) * rand(n,3));
 %% Preparing for Monte Carlo Run
 terminal_rates = zeros(n,3);
 total_time = zeros(n,1);
@@ -49,6 +48,7 @@ X_0(1) = 1;
 %% Monte Carlo Run and Output Storage
 tic
 for i = 1:n
+    fprintf("This is run number %i\n",i)
     X_0(5:7) = random_rates(i,:);
     sim_out = sim('intermediate_model',1800);
     terminal_rates(i,:) = sim_out.logsout{3}.Values.Data(end,5:7);
@@ -65,3 +65,4 @@ data_store.margin = margin;
 save(run_name, 'data_store');
 cd ../
 
+disp('Remeber! Margin saved for this run will be user input, not 1 + margin!');
