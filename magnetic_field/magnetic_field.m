7d1442d1d0d824232e731f52206ac9cc5be280de
lscharck <lscharck@gmail.com>
2022-04-23 11:48:59 -0500

    Added a time varying magnetic field to basic_model

--- magnetic_field/magnetic_field.m
+++ magnetic_field/magnetic_field.m
@@ -3,8 +3,7 @@ function b_field = magnetic_field()
 %Could implement some random bit later
 %Arbitrary B-field in inertial frame
 
-field_strength = 34000e-9; %
-field_direction = [1;0;0];
+field_strength = 34000e-9;
 
-b_field = field_strength * field_direction;
+b_field = field_strength
 end
