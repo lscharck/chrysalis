ac8c5508eea0887cd192add292d5eb98f8aa8016
JustinianOfConstantinople <jfb0029@auburn.edu>
2022-04-27 12:00:51 -0500

    Modified MMOI and MC to enable Monte Carlo variaitons with the MMOI tensor. .mat file is storage for first MMOI run.

--- model/MMOI.m
+++ model/MMOI.m
@@ -1,12 +1,38 @@
 %Mass Moment of Inertia for Aribtrary 6U Cubesat
-function I = MMOI()
+function I = MMOI(varargin)
     %Formula from Wiki
     %From CubeSat.org ...
     m = 12; % Assumed Maximum [kg]
-    x_dim = .1000; % [m]
+    x_dim = .2263; % [m]
     y_dim = .1000; % [m]
-    z_dim = .1000; % [m]
+    z_dim = .3660; % [m]
 
     I = (m/12) * diag([(y_dim^2 + z_dim^2),(x_dim^2 + z_dim^2),(x_dim^2 + y_dim^2)]);
-
+    
+    %This is a monte carlo call
+    if nargin == 1
+        %Result of random mc draw
+        draw = varargin{1};
+        delta_cg = zeros(3,1);
+        
+        %See Cubesat Spec Sheet for Ranges
+        %https://static1.squarespace.com/static/5418c831e4b0fa4ecac1bacd/t/5f24997b6deea10cc52bb016/1596234122437/CDS+REV14+2020-07-31+DRAFT.pdf
+        switch length(draw)
+            %Just varying one MMOI Tensor element - sliding along y-axis
+            case 1
+                y_range  = .02;%m
+                delta_cg(3) = (-y_range + 2 * (y_range) * draw(1));
+            %Checking out all three
+            case 3
+                ranges = [.045; .02; .07];
+                delta_cg = (-ranges + 2 * (ranges) .* draw);
+        end 
+        
+        %Taking the varied c.g. locations and tweaking the MMOI
+        delta_I = m * [(delta_cg(2)^2 + delta_cg(3)^2),delta_cg(1)^2+delta_cg(3)^2, delta_cg(1)^2 + delta_cg(2)^2];
+        
+        for i = 1:3
+           I(i,i) = I(i,i) + delta_I(i); 
+        end
+    end
 end
--- monte_carlo/monte_carlo_util.m
+++ monte_carlo/monte_carlo_util.m
@@ -1,7 +1,7 @@
 % @Author - Justin Brouillette
 % @Date - 4/26/2022
 %% Get Run Name
-prompt = {'Enter Name For Run', 'Sample Size (n = ?)', 'Margin','Run Type (Rates or MMOI)'};
+prompt = {'Enter Name For Run', 'Sample Size (n = ?)', 'Margin','Run Type (Rates Only or vary MMOI)'};
 dlgtitle = 'Run Info';
 dims = [1 40];
 definput = {'Default Name', '0', '0.0','No Run'};
@@ -9,11 +9,28 @@ answers = inputdlg(prompt,dlgtitle,dims,definput);
 run_name = answers{1};
 n = str2num(answers{2});
 margin = str2num(answers{3});
-run_type = answers{4};
+run_type = convertCharsToStrings(answers{4});
 
 if (strcmp(run_name,"Default Name") || n == 0 || strcmp(run_type,"No Run"))
-       msgbox('Can you change the responses please? We will try this again later.')  
+       msgbox('Can you change the responses please? We will try this again later.') 
+       return
 end
+
+if strcmp(run_type,"MMOI") 
+    if margin ~= 0
+        margin = 0;
+        msgbox('Going to set Margin to zero for MMOI runs. Vary one thing at a time.')
+    end
+elseif strcmp(run_type,"Rates")
+    if(margin < 0)
+        msgbox('Going to set Margin to zero, negative margin is not allowed.')
+        margin = 0;
+    end
+else
+    msgbox('Currently, acceptable run types are "Rates" or "MMOI". Please enter one of those two options.')
+    return
+end
+    
 run_name = strcat(run_name, '.mat');
 
     
@@ -35,7 +52,7 @@ b_field = magnetic_field();
 
 
 %Mass Moment of Inertia
-I = MMOI;
+I = MMOI();
 %% Initializing Rates with Random Calls
 expected_rates = deg2rad(5); %We expect 5 deg/s per axis
 % margin = 2;
@@ -51,34 +68,65 @@ X_0(1) = 1;
 
 %% Monte Carlo Run and Output Storage
 tic
-for i = 1:n
-    fprintf("This is run number %i\n",i)
-    X_0(5:7) = random_rates(i,:);
-    sim_out = sim('intermediate_model',1800);
-    terminal_rates(i,:) = sim_out.logsout{3}.Values.Data(end,5:7);
-    total_time(i) = sim_out.tout(end);
-    toc
-    
-    %% Save for Later Post Processing
-    cd run_storage
-    data_store.initial_rates = random_rates;
-    data_store.terminal_rates = terminal_rates;
-    data_store.total_time = total_time;
-    data_store.margin = margin;
-    data_store.run_type = run_type;
-    save(run_name, 'data_store');
-    cd ../
-    
+
+%A run with just rates variations
+if strcmp(run_type,"Rates") 
+    for i = 1:n
+        fprintf("This is run number %i\n",i)
+        X_0(5:7) = random_rates(i,:);
+        sim_out = sim('intermediate_model',1800);
+        terminal_rates(i,:) = sim_out.logsout{3}.Values.Data(end,5:7);
+        total_time(i) = sim_out.tout(end);
+        toc
+
+        %% Save for Later Post Processing
+        cd run_storage
+        data_store.initial_rates = random_rates;
+        data_store.terminal_rates = terminal_rates;
+        data_store.total_time = total_time;
+        data_store.margin = margin;
+        data_store.run_type = run_type;
+        save(run_name, 'data_store');
+        cd ../
+
+    end
+end
+
+%% Vary MMOI instead
+if strcmp(run_type,"MMOI")
+    MMOI_tensors = cell(n,1);
+    for i = 1:n
+        I = MMOI(rand(1)); %Right now, just varying MMOI for one axis, change to three later.
+        fprintf("This is run number %i\n",i)
+        X_0(5:7) = random_rates(i,:);
+        sim_out = sim('intermediate_model',1800);
+        terminal_rates(i,:) = sim_out.logsout{3}.Values.Data(end,5:7);
+        total_time(i) = sim_out.tout(end);
+        MMOI_tensors{i} = I;
+        toc
+
+        %% Save for Later Post Processing
+        cd run_storage
+        data_store.initial_rates = random_rates;
+        data_store.terminal_rates = terminal_rates;
+        data_store.total_time = total_time;
+        data_store.margin = margin;
+        data_store.run_type = run_type;
+        data_store.MMOI_tensors = MMOI_tensors;
+        save(run_name, 'data_store');
+        cd ../
+
+    end
 end
 
 %% Save for Later Post Processing - Moved to loop to save the pain
-cd run_storage
-data_store.initial_rates = random_rates;
-data_store.terminal_rates = terminal_rates;
-data_store.total_time = total_time;
-data_store.margin = margin;
-data_store.run_type = run_type;
-save(run_name, 'data_store');
-cd ../
-
-disp('Remeber! Margin saved for this run will be user input, not 1 + margin!');
+% cd run_storage
+% data_store.initial_rates = random_rates;
+% data_store.terminal_rates = terminal_rates;
+% data_store.total_time = total_time;
+% data_store.margin = margin;
+% data_store.run_type = run_type;
+% save(run_name, 'data_store');
+% cd ../
+% 
+% disp('Remeber! Margin saved for this run will be user input, not 1 + margin!');
