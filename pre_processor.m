4b9df48deb6d9a194a8947b1158d439f615691a3
lscharck <lscharck@gmail.com>
2022-05-13 00:12:00 -0500

    updated them

--- pre_processor.m
+++ pre_processor.m
@@ -1,6 +1,6 @@
 %% fetch and proces data
 
-sim_out = sim('basic_model', 2500); % to get data from the model
+sim_out = sim('basic_model', 200); % to get data from the model
 %f = fopen('Data_out.dat', 'w+');
 
 time = sim_out.logsout{3}.Values.Time;
@@ -8,7 +8,8 @@ mu = sim_out.logsout{3}.Values.Data(:, 1);
 eta1 = sim_out.logsout{3}.Values.Data(:, 2);
 eta2 = sim_out.logsout{3}.Values.Data(:, 3);
 eta3 = sim_out.logsout{3}.Values.Data(:, 4);
-quat = [mu eta1 eta2 eta3];
+fire = sim_out.logsout{1}.Values.Data;  
+quat = [mu eta1 eta2 eta3 fire];
 quat_sparse = quat(1, :);
 
 dt = 1;
@@ -34,4 +35,8 @@ while i <= length(time)
     
 end
 
-eul = quat2eul(quat_sparse); % save me to a file please :)
+eul = quat2eul(quat_sparse(:, 1:4)); % save me to a file please :)
+eul = [eul, quat_sparse(:, 5:end)];
+
+%% Okay - Save
+save('eulers', 'eul')
--- randomAngleRotation.m
+++ randomAngleRotation.m
@@ -2,16 +2,21 @@ close all
 clear all
 clc
 
-M = readmatrix('sample_eulers.xlsx');
+M = load('eulers');
+M = M.eul;
 % save as matlab variable
 
 %% Define cube
 H=[-10 10 -10 10 -10 10 -10 10; -5 -5 5 5 -5 -5 5 5; -17.025 -17.025 -17.025 -17.025 17.025 17.025 17.025 17.025]; % Vertices of the 6U cubesat in cm
 S=[1 2 4 3; 1 2 6 5; 1 3 7 5; 3 4 8 7; 2 4 8 6; 5 6 8 7]; % Surfaces of the cube
-
-
+% 1 = z
+% 2 = y
+% 3 = x
 %% Plot of rotation cube
 figure(1)
+[X,Y,Z] = meshgrid(-20:5:20,-20:5:20,-20:5:20);
+U = abs(X);
+V = 0*Y;
 for i = 1:length(M)
     %x = M(i,4)
     %t = x + 1
@@ -43,14 +48,27 @@ for i = 1:length(M)
         
         Si=S(k,:); 
         
-        fill3(H1(1,Si),H1(2,Si),H1(3,Si),'b','facealpha',0.6)
+        if k == 1 || k == 2 || k == 3
+            if M(i, 7 - k) == 1
+                color_id = 'g';
+            elseif M(i, 7 - k) == -1
+                color_id = 'r';
+            else
+                color_id = 'b';
+            end
+        else
+            color_id = 'b';
+        end
+
+        fill3(H1(1,Si),H1(2,Si),H1(3,Si),color_id,'facealpha',0.6)
 
         axis([-40 40 -40 40 -40 40])
         hold on
     end
-    %drawnow
-    %pause(1)
-    filename = ['./VideoViz/Donahue', num2str(i, '%015.0f')]; 
-    print(gcf, filename, '-r300', '-dpng')
+    quiver3(X,Y,Z,U,V,V)
+    drawnow
+    pause(0.25)
+    %filename = ['./VideoViz/Donahue', num2str(i, '%015.0f')]; 
+    %print(gcf, filename, '-r300', '-dpng')
     hold off
 end
